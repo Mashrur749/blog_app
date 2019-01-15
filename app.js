9bea20d46fcd975190ebb1ff19a5ba3bacd7818f
Musaddiqur Rahman <mashrur749@gmail.com>
2019-01-14 22:18:12 -0500

    database connection fixed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express           = require('express'),
     methodOverride    = require('method-override'),
     exrpressSanitizer = require('express-sanitizer');
 
-var DataBase = "mongodb://localhost:27017/blogsite" || "mongodb://MusaddiqurRahman:asdASD123@ds157544.mlab.com:57544/blogsite";
+var DataBase =  "mongodb://MusaddiqurRahman:asdASD123@ds157544.mlab.com:57544/blogsite" || "mongodb://localhost:27017/blogsite";
 mongoose.connect(DataBase);
 //app config
 app.set('view engine','ejs');
